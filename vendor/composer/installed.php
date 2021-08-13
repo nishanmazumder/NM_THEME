87a3fc7f89b80f7e0058bd050707ac9ac4e26c5c
nishanmazumder <arosh019@gmail.com>
2021-08-13 15:36:24 +0600

    reset

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a8a9a563c03b548fae211f2d96418f561116de7d',
+    'name' => 'wcs/nm_theme',
+  ),
+  'versions' => 
+  array (
+    'wcs/nm_theme' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8a9a563c03b548fae211f2d96418f561116de7d',
+    ),
+  ),
+);
