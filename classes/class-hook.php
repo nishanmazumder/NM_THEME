1fd9283183bbd693cceab178019e9345a7caf509
nishanmazumder <arosh019@gmail.com>
2021-08-20 21:53:27 +0600

    add options on custom post type

--- classes/class-hook.php
+++ classes/class-hook.php
@@ -27,6 +27,10 @@ class Hook
 
         //Title change for all post
         //add_filter('the_title', 'title_change', 10, 1);
+
+        //Add column on custom post type - Projects
+        add_filter('manage_add_project_posts_columns', [$this, 'nm_add_taxonomy_column']);
+        add_action('manage_posts_custom_column', [$this, 'nm_add_taxonomy_column_functions'], 10, 2);
     }
 
     public function nm_theme_cf7_operator_active($options)
@@ -42,7 +46,41 @@ class Hook
         return $options;
     }
 
-    public function title_change($title){
+    public function title_change($title)
+    {
         return "Change $title with filter";
+    }
+
+    public function nm_add_taxonomy_column($columns)
+    {
+
+        $columns = [
+            'cb' => $columns['cb'],
+            'title' => __('Title', 'nm_theme'),
+            'taxonomy' => __('Taxonomy', 'nm_theme'),
+            'thumbnail' => __('Thumbnail', 'nm_theme'),
+            'shortcode' => __('Shortcode', 'nm_theme'),
+            'author' => __('Author', 'nm_theme')
+        ];
+
+        return $columns;
+    }
+
+    public function nm_add_taxonomy_column_functions($columns, $post_id)
+    {
+        switch ($columns) {
+            case 'taxonomy':
+                echo strip_tags(get_the_term_list($post_id, 'project_type'));
+                break;
+            case 'thumbnail':
+                echo get_the_post_thumbnail($post_id, [32, 32]);
+                break;
+            case 'shortcode': //On Shorcode Class
+                echo '[add_project id="'.get_the_ID().'" title="'.get_the_title().'"]';
+                break;
+            default:
+                echo "Nothing found";
+                break;
+        }
     }
 }
--- classes/class-shortcode.php
+++ classes/class-shortcode.php
@@ -24,6 +24,9 @@ class Shortcode
     {
         //Shortcode for simple text
         add_shortcode('nm_title_des', [$this, 'add_title_des_shortcode']);
+
+        //Shortcode for new custom post type - Projects
+        add_shortcode('add_project', [$this, 'add_project_shortcode']);
     }
 
     public function add_title_des_shortcode($atts)
@@ -34,5 +37,21 @@ class Shortcode
         ], $atts, 'nm_title_des');
 
         echo sprintf("%s", $atts['title']);
+    }
+
+    public function add_project_shortcode($atts)
+    {
+        $atts = shortcode_atts([
+            'id' => '1',
+            'title' => 'Project Title'
+        ], $atts, 'add_project');
+
+        $data = '';
+        $data .= get_the_title($atts['id']);
+        $data .= get_the_post_thumbnail($atts['id']);
+        $data .= get_the_excerpt($atts['id']);
+
+        return $data;
+        //return print_r($atts);
     }
 }
